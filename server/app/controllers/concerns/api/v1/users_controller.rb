722415b6535600b5983d90074c6e25d4ef207cf6
dchase87 <david.chase4@gmail.com>
2018-03-15 20:49:26 -0400

    add auth controller and user strong params

--- server/app/controllers/concerns/api/v1/users_controller.rb
+++ server/app/controllers/concerns/api/v1/users_controller.rb
@@ -7,6 +7,26 @@ module API
         render json: User.find_by(id: params[:id])
       end
 
+      def create
+        user = User.new(userParams)
+        if user.save
+          created_jwt = issue_token({id: user.id})
+          render json: {
+            id: user.id,
+            jwt: created_jwt
+          }
+        else
+          render json: {
+            error: 'Something went wrong. Please try again.'
+          }, status: 404
+        end
+      end
+
+      private
+
+      def userParams
+        params.require(:email, :password, :password_confirmation).permit(:first_name, :last_name, :bank, :bank_acct_no)
+      end
 
     end
   end
